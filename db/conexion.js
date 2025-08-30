50b4b894af1764dc370229eefd8e1fd2fbe93eb0
facundoLozano0 <faculozano.ar.1@gmail.com>
2025-08-30 15:47:06 -0300

    de nuevo conexion

--- db/conexion.js
+++ db/conexion.js
@@ -1,11 +1,17 @@
-require('dotenv').config();
 const mysql = require('mysql2');
+require('dotenv').config();
 
-const connection = mysql.createConnection(process.env.DATABASE_URL);
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
 
 connection.connect((err) => {
   if (err) {
-    console.error('Error de conexión a MySQL:', err);
+    console.error('❌ Error al conectar:', err);
     return;
   }
   console.log('✅ Conectado a la base de datos MySQL');
